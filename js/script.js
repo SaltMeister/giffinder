294f4109ff93dd33755d47b64e4a308bdd07d979
Simon Huang <huisimonliu@gmail.com>
2018-03-01 01:17:44 +0000

    added the functionality of the javascript code

--- js/script.js
+++ js/script.js
@@ -9,39 +9,44 @@ $(document).ready(function(){
     1. click handler function
     2. get the typed input from the search input box
     3. call the functions below!
-  */
+  */$("button").click(function() {
+    var input = $("input").val();
+    callGiphyAPIWithSearchTerm(input);
+    });
+    $("input").keypress(function(e) {
+      if(e.which === 13) {
+        $("button").click();    
+      } 
+    });
   
   function giphyURLWithSearchTerm(searchTerm) {
-      // write a function that will return a url for the giphy API with
-      // the searchTerm provided in the parameters
-       var firstPart = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "";
-    var secondPart = "&api_key=dc6zaTOxFJmzC";
-    //var newUrl = firstpart + secondPart;
-  }
+          // write a function that will return a url for the giphy API with
+    // the searchTerm provided in the parameters
+    var giphyURL = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+     return giphyURL;
+}
+
 
   function appendImageToGallery(srcURL) {
       // write a function that will append an <img> to the div with class="gallery"
       // using the URL provided in the parameters
-       $("body").append("<img src=" +srcURL+"></img>");
+       $(".gallery, .row").append("<img src=" +srcURL+"></img>");
   }
 
   function callGiphyAPIWithSearchTerm(searchTerm) {
-      // use $.ajax to call the giphy api with the given search term from the parameters.
-      // get the first image url from the ajax response
-      // use appendImageToGallery to put the image onto the screen
-       $.ajax({
-      url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+          // use the giphyURLWithSearchTerm function to customize the url below
+    var giphyURL = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+      url: giphyURL,
       method: "GET",
       success: function(response) {
-           // Log the whole response to the console
-            /// console.log(response);
-           // Log the first image of the data to the console
-            //  console.log(response.data[0].url);
-           // Log the "type" property of the first image object to the console
-                console.log(response.data[0].type);
-           // Log the "title" property of the first image object to the console
-                console.log(response.data[0].title);
+           var random = Math.floor(Math.random()*5);
+           var url = response.data[random].images.original.url;
+           appendImageToGallery(url);
+           $("body").append(giphyURLWithSearchTerm(searchTerm));
+          
       },
+      
     }); 
   }
 });
